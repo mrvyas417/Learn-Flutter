15968220ff82ae89c7ca13ee516c9d97e0d9ba9d
shubham.vyas <shubham.vyas@sunarctechnologies.com>
2021-12-29 11:38:31 +0530

    day 13 done

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_learn/Widget/drawer.dart';
+import 'package:flutter_learn/Widget/item_widget.dart';
+import 'package:flutter_learn/models/catalog.dart';
 
 // ignore: must_be_immutable
 class Home extends StatelessWidget {
@@ -10,14 +12,23 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.blueGrey,
         title: Text("E cart"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(name),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummylist[index],
+            );
+          },
+        ),
       ),
       drawer: Mydrawer(),
     );
